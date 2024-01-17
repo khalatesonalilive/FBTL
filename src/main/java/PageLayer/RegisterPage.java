703401554f42cac3c00b816b143d018f0668ab23
khalatesonalilive <khalatesonali1234@gmail.com>
2024-01-17 17:47:20 +0530

    First and Last name TC are added

--- src/main/java/PageLayer/RegisterPage.java
+++ src/main/java/PageLayer/RegisterPage.java
@@ -1,11 +1,19 @@
 package PageLayer;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import BaseLayer.BaseClass;
 
 public class RegisterPage extends BaseClass{
 
+	@FindBy(name="firstname")
+	private WebElement fname ;
+	
+	@FindBy(name="lastname")
+	private WebElement lname;
+	
 	public RegisterPage()
 	{
 		PageFactory.initElements(driver, this);
@@ -17,4 +25,19 @@ public class RegisterPage extends BaseClass{
 		
 	return 	driver.getCurrentUrl();
 	}
+	
+	public void FirstAndLastNameFunctionality(String FirstN, String LastN)
+	
+	{
+		fname.sendKeys(FirstN);
+		lname.sendKeys(LastN);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
